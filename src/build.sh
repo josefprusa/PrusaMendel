4910ae30337d8210e55d340acda06aa9159ab39c
jglauche <jglauche@cloud9.(none)>
2012-02-18 00:26:39 +0100

    testing build

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+mkdir build
+cd build
+mkdir stl
+cd ..
 
+cp COPYING build/
 
-openscad -s metric-prusa/frame-vertex.stl -D 'basefoot=false' source/frame-vertex.scad
+openscad -s build/stl/frame-vertex.stl -D 'basefoot=false' parts/frame-vertex.scad
+
 
